efd4b7c5679930df5cccea52a380043570b7dcea
ErlNievera <erl.nievera.college@ustangelicum.edu.ph>
2024-12-14 14:38:03 +0800

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-celsius = float(input("Enter value in celsius: "))
-print(f"{celsius} celsius is equivalent to {(1.8 * celsius) + 32} farenheit")
-
-fahrenheit = float(input("Enter value in fahrenheit: "))
-print(f"{fahrenheit} celsius is equivalent to {(0.56 * fahrenheit) - 32} celsicus")
